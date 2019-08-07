0cd89cb84e3cc429ee13da680e9cdf923fc18ffa
阮伟标 <ruanweibiao@chinatelecom.cn>
2019-08-07 10:27:30 +0800

    添加palybook版本管理接口

--- goout/job/filemanager.pb.gw.go
+++ goout/job/filemanager.pb.gw.go
@@ -110,7 +110,7 @@ func RegisterFileManagerHandler(ctx context.Context, mux *runtime.ServeMux, conn
 // "FileManagerClient" to call the correct interceptors.
 func RegisterFileManagerHandlerClient(ctx context.Context, mux *runtime.ServeMux, client FileManagerClient) error {
 
-	mux.Handle("PUT", pattern_FileManager_UploadPlaybook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_FileManager_UploadPlaybook_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -134,7 +134,7 @@ func RegisterFileManagerHandlerClient(ctx context.Context, mux *runtime.ServeMux
 }
 
 var (
-	pattern_FileManager_UploadPlaybook_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "filemanager", "upload_playbook"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_FileManager_UploadPlaybook_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "filemanager", "uploadPlaybook"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
